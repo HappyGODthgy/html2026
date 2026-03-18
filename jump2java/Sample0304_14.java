2f03d463d057e18b454c8ea1d7c218ade04047b3
HappyGODthgy <daejung1624@dge.go.kr>
2026-03-18 10:36:27 +0900

    Add files via upload

---
+++ jump2java/Sample0304_14.java
@@ -0,0 +1,10 @@
+package jump2java;
+
+public class Sample0304_14 {
+    public static void main(String[] args) {
+    	
+    	
+        System.out.println(String.format("I have  apples", 3));  // "I have 3 apples" 출력
+        System.out.println(String.format("rate is ", 3.234));  // "rate is 3.234" 출력
+    }
+}
